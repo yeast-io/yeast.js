2db3a5c009433206c38147c04ba512531415fa12
danielsss <better.sunjian@gmail.com>
2024-07-10 00:09:39 +1000

    Removed: compiling files

--- packages/cli/commands/bittorrent.d.ts
+++
@@ -1,9 +0,0 @@
-import { IConfig } from './config.js';
-import { QBittorrent } from '@ctrl/qbittorrent';
-declare class Bittorrent {
-    protected config: IConfig;
-    protected client: QBittorrent;
-    constructor(config: IConfig);
-    add(torrentId: string | number): Promise<void>;
-}
-export default Bittorrent;
--- packages/cli/utils.d.ts
+++
@@ -1,3 +0,0 @@
-type Mixed = null | undefined | string | any[] | object | number | Map<any, any> | Set<any> | Buffer;
-export declare const isEmpty: (value: Mixed) => boolean;
-export {};
